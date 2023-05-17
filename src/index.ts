f2281af127f6846627f22e1ed7dcf12ff4360242
Marsel Brandao Junior <marseljrdev@gmail.com>
2023-05-17 20:27:35 -0300

    Criando a primeira rota

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("api growdev");
+import express, { Request, Response } from "express";
+import { Growdev } from "./models/growdever";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("Acessou a rota...");
+  const growdev = new Growdev(5, 'Junior', 25);
+  res.status(200).send({ success: true, data: growdev.toJson() });
+});
+
+app.listen(3333, () => {
+  console.log("Api is running port 3333");
+});
---
+++ src/models/growdever.ts
@@ -0,0 +1,28 @@
+export class Growdev {
+  constructor(
+    private _id: number,
+    private _nome: string,
+    private _idade: number
+  ) {}
+
+  public get id() {
+    return this._id;
+  }
+
+  public get nome() {
+    return this._nome;
+  }
+
+  public get idade() {
+    return this._idade;
+  }
+
+  public toJson(){
+    return {
+        // id: this._id,
+        nome: this._nome,
+        idade: this._idade
+    }
+  }
+  
+}
